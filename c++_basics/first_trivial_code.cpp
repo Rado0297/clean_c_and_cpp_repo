34e375d4bf40f5a05385167a9d034e60f26bbca6
Radoslav Radkov <radoslav@Radoslavs-MacBook-Pro.local>
2024-12-09 12:28:38 +0200

    Initial commit for C/C++ skills and codes

---
+++ c++_basics/first_trivial_code.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Print Hello, world!
+    cout << "Hello, world!" << endl;
+
+    return 0;
+}
---
+++ main_test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "First C++ code compiled on mac\n";
+
+    return 0;
+}
